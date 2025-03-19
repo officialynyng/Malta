017eb506db87d9a6a9f269334204045bd0e3799d
ynyng <ynyng@ynyng.org>
2025-03-19 17:40:19 -0500

    Update MaltaBot.py

--- MaltaBot.py
+++ MaltaBot.py
@@ -7,7 +7,7 @@ import os
 TOKEN = os.getenv("TOKEN")
 GUILD_ID = int(os.getenv("GUILD_ID"))
 APPROVED_ROLE_NAME = os.getenv("APPROVED_ROLE_NAME")
-OWNER_ID = YOUR_DISCORD_ID_HERE
+OWNER_ID = int(os.getenv("OWNER_ID"))
 WELCOME_CHANNEL_ID = int(os.getenv("WELCOME_CHANNEL_ID"))  # Set this in your environment variables
 INTENTS = discord.Intents.default()
 INTENTS.messages = True
