963f1ef25027a7ad52c229ce436c429b3e14a471
ynyng <ynyng@ynyng.org>
2025-03-21 12:21:32 -0500

    Update exp_background.py

--- cogs/exp_background.py
+++ cogs/exp_background.py
@@ -252,7 +252,7 @@ class CRPGGroup(app_commands.Group):
 
         last_activity = user_data['last_message_ts']
         daily_multiplier = user_data['daily_multiplier']  # Using the daily multiplier
-        retirement_multiplier = user_data['retirement_multiplier']  # Using the retirement multiplier
+        retirement_multiplier = user_data['multiplier']  # Using the retirement multiplier
         time_since_last_activity = current_time - last_activity
 
         if time_since_last_activity < TIME_DELTA:
