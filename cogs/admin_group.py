b91edc5faa90acdde1ba9ee34da5fedd91bd27ec
ynyng <ynyng@ynyng.org>
2025-03-22 16:28:53 -0500

    Update admin_group.py

--- cogs/admin_group.py
+++ cogs/admin_group.py
@@ -9,6 +9,9 @@ from discord import (app_commands,)
 from cogs.exp_engine import (on_user_comment,)
 from cogs.exp_utils import (get_all_user_ids, get_user_data, update_user_data,)
 from cogs.admin_config import (APPROVED_ROLE_NAME, OWNER_ID, GUILD_ID)
+from cogs.exp_config import (
+    db
+)
 
 class AdminGroup(app_commands.Group):
     def __init__(self, bot):
