549c4fdbee9bf00fb47623a5600917cd8e09549f
ynyng <ynyng@ynyng.org>
2025-06-03 20:45:53 -0500

    Update ui_base.py

---
+++ cogs/x_utilities/ui_base.py
@@ -0,0 +1,14 @@
+# utils/ui_base.py 
+
+import discord
+
+class BaseCogView(discord.ui.View):
+    def __init__(self, *, cog, timeout=None):
+        super().__init__(timeout=timeout)
+        self.cog = cog
+
+class BaseCogButton(discord.ui.Button):
+    def __init__(self, *, user_id, cog, **kwargs):
+        super().__init__(**kwargs)
+        self.user_id = user_id
+        self.cog = cog
