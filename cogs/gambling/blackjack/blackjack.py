b9e3958ddef1e91db35f12670ecdfdbab097d30a
ynyng <ynyng@ynyng.org>
2025-06-03 22:17:54 -0500

    Update blackjack.py

--- cogs/gambling/blackjack/blackjack.py
+++ cogs/gambling/blackjack/blackjack.py
@@ -139,7 +139,7 @@ class BlackjackGameView(BaseCogView):
         self.clear_items()
         self.add_item(BackToGameButton(self.user_id, self.parent, self.cog))
         self.add_item(PlayAgainButton(self.user_id, parent_view=self.parent, game_key="blackjack", bet=self.bet))
-        self.add_item(RefreshGoldButton(self.user_id))
+        self.add_item(RefreshGoldButton())
         await interaction.response.edit_message(embed=self.get_embed(reveal_dealer=True, final=True), view=self)
 
 class HitButton(Button):
